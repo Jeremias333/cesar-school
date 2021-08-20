e204507d657194d926f238f0a5bdf5ef5ff91d93
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-08-20 11:20:50 -0300

    initialize variables with 0

--- alg-est/cedulas.c
+++ alg-est/cedulas.c
@@ -2,47 +2,59 @@
 
 int main() {
     double val, result;
-    int cel_100, cel_50, cel_20, cel_10, cel_5, cel_2 = 0;
-    int moe_1, moe_50, moe_25, moe_10, moe_5, moe_01 = 0;
+    int cel_100 = 0;
+    int cel_50 = 0;
+    int cel_20 = 0;
+    int cel_10 = 0;
+    int cel_5 = 0;
+    int cel_2 = 0;
+
+    int moe_1 = 0;
+    int moe_50 = 0;
+    int moe_25 = 0;
+    int moe_10 = 0;
+    int moe_5 = 0; 
+    int moe_01 = 0;
 
     scanf("%lf", &val);
     printf("valor digitado: %.2lf\n", val);
 
     while (1){
+        printf("Passou");
         if(val >= 100.0){
             cel_100 += 1;
             val = val - 100.0;
-        }else if (val >= 50.0){
+        }else if (val >= 50.0 && val < 100.0){
             cel_50 += 1;
             val = val - 50.0;
-        }else if(val >= 20.0){
+        }else if(val >= 20.0 && val < 50.0){
             cel_20 += 1;
             val = val - 20.0;
-        }else if(val >= 10){
+        }else if(val >= 10 && val < 20.0){
             cel_10 += 1;
             val = val - 10.0;
-        }else if(val >= 5){
+        }else if(val >= 5 && val < 10.0){
             cel_5 += 1;
             val = val - 5.0;
-        }else if(val >= 2){
+        }else if(val >= 2 && val < 5.0){
             cel_2 += 1;
             val = val - 2.0;
-        }else if(val >= 1){
+        }else if(val >= 1 && val < 2.0){
             moe_1 += 1;
             val = val - 1.0;
-        }else if(val >= 0.50){
+        }else if(val >= 0.50 && val < 1.0){
             moe_50 += 1;
             val = val - 0.50;
-        }else if(val >= 0.25){
+        }else if(val >= 0.25 && val < 0.50){
             moe_25 += 1;
             val = val - 0.25;
-        }else if(val >= 0.10){
+        }else if(val >= 0.10 && val < 0.25){
             moe_10 += 1;
             val = val - 0.10;
-        }else if(val >= 0.5){
+        }else if(val >= 0.5 && val < 0.10){
             moe_5 += 1;
             val = val -0.05;
-        }else if(val >= 0.01){
+        }else if(val >= 0.01 && val < 0.5){
             moe_01 += 1;
             val = val - 0.01;
         }else{

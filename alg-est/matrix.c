e5c69a4065e37188b315527d81a4770483a17b02
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-08-30 23:00:04 -0300

    commit

--- alg-est/matrix.c
+++ alg-est/matrix.c
@@ -6,15 +6,29 @@ int main(){
     //scanf(" %s", in);
     gets(in);
 
-    printf("%c %c", in[0], in[2]);
-
     int line = in[0] - '0';
     int col = in[2] - '0';
 
+    // int sizeMatrix = line*col;
+
+    // printf("tamanho matriz: %d\n", sizeMatrix);
+
     int matrix[line][col];
 
-    printf("linha: %d\n", line);
-    printf("coluna: %d\n", col);
+    // printf("linha: %d\n", line);
+    // printf("coluna: %d\n", col);
     
-    
+    for(int i = 0; i < line; i++){
+        for(int j = 0; j < col; j++){
+            // printf("linha: %d \ncoluna: %d", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // for(int i = 0; i < line; i++){
+    //     for(int j = 0; j < col; j++){
+    //         printf("%d ", matrix[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 }

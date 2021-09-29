846461cbe4726340649a7e27b161582377b455e3
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-09-28 22:46:19 -0300

    one alter

--- alg-est/jos_prova1/q1_jos.c
+++ alg-est/jos_prova1/q1_jos.c
@@ -9,21 +9,39 @@ int main(void)
 			scanf("%d", &matrix_triangle[i][j]);
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-			printf("%d ", matrix_triangle[i][j]);
-		printf("\n");
-	}
+	//for (int i = 0; i < 3; i++) {
+	//	for (int j = 0; j < 3; j++)
+	//		printf("%d ", matrix_triangle[i][j]);
+	//	printf("\n");
+	//}
 
 	//Buscando triangulo inferior
 	int inferTrue = 1;
 
 	for (int i = 0; (i < 3) && (inferTrue == 1); i++) {
 		for (int j = (i + 1); (j < 3) && (inferTrue == 1); j++) {
-			if (matrix_triangle[i][j] != 1)
+			if (matrix_triangle[i][j] != 0)
 				inferTrue = 0;
 		}
 	}
 
-	printf("É inferior? %d", inferTrue);
+	//Buscando triangulo superior
+	int superTrue = 1;
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (i < j && matrix_triangle[i][j] != 0)
+				superTrue = 0;
+		}
+	}
+	// printf("É superior? %d", superTrue);
+
+	if (inferTrue == 1){
+		printf("Por baixo");
+	}else if(superTrue){
+		prinf("Por cima");
+	}else {
+		printf("Nao pode atravessar");
+	}
+
 }

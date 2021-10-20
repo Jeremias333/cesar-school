8c3109f2c5d92920a5f9c2e29c0222e67183fb53
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-10-20 13:21:53 -0300

    modify files

--- alg-est/2-1quest/q1_jos.c
+++ alg-est/2-1quest/q1_jos.c
@@ -2,11 +2,41 @@
 #include "stdio.h"
 #include "operaArray.h"
 
+int array[];
+
+void create_array(){
+
+
+    //pedir o array de números ao usuário por aqui
+}
 
 int main(void){
 
+    create_array();
+
+    int loop = 1;
+    char choice = ' ';
+
+    while(1){
+        printf("Escolha uma das opções abaixo:\nR - Inverter a lista\nE - Contar elemento\nU - Elementos únicos\nD - Deletar uma posição\nS - Sair\n");
+        scanf("%c", &choice);
+        
+        if(choice == 'R' || choice == 'r'){
+
+        }else if(choice == 'E' || choice == 'e'){
+
+        }else if(choice == 'U' || choice == 'u'){
+
+        }else if(choice == 'D' || choice == 'd'){
+
+        }else if(choice == 'S' || choice == 's'){
+
+        }else{
+            printf("Opção inválida...");
+        }
+
+
+    }
     
-
-
     return 0;
 }

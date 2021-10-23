afc240949c19b8b4895c09e8fe46ac1fd9e22612
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-10-23 13:03:25 -0300

    added updates

--- alg-est/2-1quest/q1_jos.c
+++ alg-est/2-1quest/q1_jos.c
@@ -2,25 +2,38 @@
 #include "stdio.h"
 #include "operaArray.h"
 
-int array[];
+int* create_array(){
+    int size = 0;
 
-void create_array(){
+    printf("Digite o tamanho do seu array: ");
+    scanf("%d", &size);
 
+    int * array = (int *) calloc (size, sizeof (int));
 
-    //pedir o array de números ao usuário por aqui
+    for(int index = 0; index < size; index++) {
+        scanf("%d",  &array[index]);
+    }
+
+    return array;
 }
 
 int main(void){
 
-    create_array();
+    int *array = create_array();
+
+    // printf("%d ", array[0]);
 
     int loop = 1;
-    char choice = ' ';
+    char choice;
 
-    while(1){
-        printf("Escolha uma das opções abaixo:\nR - Inverter a lista\nE - Contar elemento\nU - Elementos únicos\nD - Deletar uma posição\nS - Sair\n");
-        scanf("%c", &choice);
+    while(loop){
         
+        printf("Escolha uma das opções abaixo:\nR - Inverter a lista\nE - Contar elemento\nU - Elementos únicos\nD - Deletar uma posição\nS - Sair\n");
+        // choice = getchar();
+        // fflush(stdin);
+        scanf(" %c", &choice);
+        // printf("%c", choice);
+
         if(choice == 'R' || choice == 'r'){
 
         }else if(choice == 'E' || choice == 'e'){
@@ -30,13 +43,12 @@ int main(void){
         }else if(choice == 'D' || choice == 'd'){
 
         }else if(choice == 'S' || choice == 's'){
-
+            exit(0);
         }else{
-            printf("Opção inválida...");
+            system("clear");
+            printf("Opção inválida...\n");
         }
-
-
     }
-    
+
     return 0;
 }

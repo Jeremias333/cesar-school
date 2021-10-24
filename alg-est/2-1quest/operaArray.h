e0a0dce7f6d5be88c92f29b71c0b510205d91f8e
Jeremias Oliveira <jeremiasprogrammer@gmail.com>
2021-10-23 22:09:06 -0300

    updated files

--- alg-est/2-1quest/operaArray.h
+++ alg-est/2-1quest/operaArray.h
@@ -1,24 +1,103 @@
 #ifndef OPERAARRAY
 #define OPERAARRAY
 
-int * newReverse(int list_array[]){
-    int array[20];
-    return array;
+int * newReverse(int * list_array, int size){
+    // printf("%d tamanho do array", size);
+    
+    int interator = 0;
+    int aux;
+    int right = size-1;
+    int left = 0;
+
+    while(1){
+        //sai do loop quando concluir todos números e a quantidade for impar de elementos
+        if(left == right){
+            break;
+        }
+
+        aux = list_array[left];
+        list_array[left] = list_array[right];
+        list_array[right] = aux;
+
+        left++;
+        right--;
+
+        interator++;
+
+        //sai do loop quando concluir todos números e a quantidade for par de elementos
+        if(interator == size/2){
+            break;
+        }
+    }
+
+    // for(int i = 0; i < size; i++){
+    //     printf("%d ", list_array[i]);
+    // }
+
+    return list_array;
 }
 
-int elemNum(int x){
+int elemNum(int * list_array, int size, int x){
+    int cont = 0;
 
-    return 0;
+    for(int i = 0; i < size; i++){
+        if (x == list_array[i]) {
+            cont++;
+        }
+    }
+
+    return cont;
 }
 
-int * unique(int list_array[]){
-    int array[20];
-    return array;
+int * unique(int * list_array, int size){
+    int cont = 0;
+    int list_array_copy[size];
+
+    //copiando valores do array
+    for (int i = 0; i < size; i++){
+        list_array_copy[i] = list_array[i];
+    }
+
+    //utilizando um algoritmo reversivo para poder transformar os números repetidos em -1
+    for (int i = 0; i <= size; i++){
+        for (int j = i+1; j < size; j++){
+            if(list_array[i] == list_array[j]){
+                list_array_copy[i] = -1;
+                list_array_copy[j] = -1;
+            }
+        }
+    }
+
+    //contando números repetidos
+    for (int i = 0; i < size; i++){
+        if (list_array_copy[i] != -1){
+            cont++;
+        }
+    }
+
+    //criando array que será utilizado para unir os números únicos
+    int * list_array_final = malloc(cont * sizeof(int));
+
+    //criando variavel de controle para popular numeros unicos no array
+    int cont_fixed = cont;
+
+    //unindo em um array apenas os números que não se repetem.
+    for(int i = 0; i < size; i++){
+        if(list_array_copy[i] != -1 && cont >= 0){
+            list_array_final[cont_fixed-cont] = list_array_copy[i];
+            cont--;
+        }
+    }
+
+    //enviando o tamanho do array
+    list_array_final[-1] = cont_fixed;
+
+    return list_array_final;
+    
 }
 
 void deleteN(int pos){
 
-    
 }
 
 #endif
--- alg-est/2-1quest/q1_jos.c
+++ alg-est/2-1quest/q1_jos.c
@@ -2,15 +2,31 @@
 #include "stdio.h"
 #include "operaArray.h"
 
-int* create_array(){
-    int size = 0;
+FILE *myFile;
 
-    printf("Digite o tamanho do seu array: ");
-    scanf("%d", &size);
+int write_file(){
+    char choice;
+    printf("\nDeseja escrever no arquivo o resultado? \nS - Sim\nN - Não\n");
+    scanf(" %c", &choice);
 
-    int * array = (int *) calloc (size, sizeof (int));
+    if (choice == 'S' || choice == 's'){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void addToOutput(char text){
+    myFile = fopen("resultados.txt", "a");
+    fprintf(myFile, "%c", text);
+    fclose(myFile);
+}
+
+int* create_array(int size){
+    int * array = malloc(size * sizeof(int));
 
     for(int index = 0; index < size; index++) {
+        printf("Digite o %dº valor: ", index+1);
         scanf("%d",  &array[index]);
     }
 
@@ -18,31 +34,94 @@ int* create_array(){
 }
 
 int main(void){
+    int size = 0;
 
-    int *array = create_array();
+    //limpando documento de relatório
+    myFile = fopen("resultados.txt", "w");
+    fprintf(myFile, "%s", "");
+    fclose(myFile);
 
-    // printf("%d ", array[0]);
+    printf("Digite o tamanho do seu array: ");
+    scanf("%d", &size);
+
+    int *array = create_array(size);
 
     int loop = 1;
     char choice;
 
     while(loop){
         
-        printf("Escolha uma das opções abaixo:\nR - Inverter a lista\nE - Contar elemento\nU - Elementos únicos\nD - Deletar uma posição\nS - Sair\n");
-        // choice = getchar();
-        // fflush(stdin);
+        printf("\nEscolha uma das opções abaixo:\nR - Inverter a lista\nE - Contar elemento\nU - Elementos únicos\nD - Deletar uma posição\nS - Sair\n");
         scanf(" %c", &choice);
-        // printf("%c", choice);
 
         if(choice == 'R' || choice == 'r'){
+            myFile = fopen("resultados.txt", "a");
+            
+            array = newReverse(array, size);
+            char value;
+            printf("Valores reverso: ");
+            for(int i = 0; i < size; i++){
+                printf("%d ", array[i]);
+            }
+            
+            if(write_file() == 1){
+                fprintf(myFile, "%s", "Resultado de valores reversos: ");
+                for(int i = 0; i < size; i++){
+                    value = array[i]+'0';
+                    fprintf(myFile, "%c", value);
+                }
+                fprintf(myFile, "%s", "\n");
+                fclose(myFile);
 
+                printf("Resultado salvo no arquivo!");
+            }
+            printf("\n");
         }else if(choice == 'E' || choice == 'e'){
+            myFile = fopen("resultados.txt", "a");
+            int num = 0;
+            printf("Digite o valor que você busca no vetor: ");
+            scanf("%d", &num);
+            int result = elemNum(array, size, num);
+            printf("\nA quantidade de vezes que o número %d aparece no vetor é: %d\n\n", num, result);
 
+            if(write_file() == 1){
+                fprintf(myFile, "%s", "Resultado da quantidade de vezes que o número ");
+                fprintf(myFile, "%d", num);
+                fprintf(myFile, "%s", " aparece: ");
+                fprintf(myFile, "%d", result);
+                fprintf(myFile, "%s", "\n");
+                fclose(myFile);
+
+                printf("Resultado salvo no arquivo!");
+            }
+            printf("\n");
         }else if(choice == 'U' || choice == 'u'){
+            myFile = fopen("resultados.txt", "a");
+            int value_size = 0;
+            int *array2 = unique(array, size);
+            value_size = array2[-1];
 
+            printf("Valores únicos do vetor: ");
+
+            for (int i = 0; i < value_size; i++){
+                printf("%d ", array2[i]);
+            }
+
+            if(write_file() == 1){
+                fprintf(myFile, "%s", "Resultado dos números únicos do vetor: ");
+                for (int i = 0; i < value_size; i++){
+                    fprintf(myFile, "%d", array2[i]);
+                }
+                fprintf(myFile, "%s", "\n");
+                fclose(myFile);
+
+                printf("Resultado salvo no arquivo!");
+            }
+            printf("\n");
         }else if(choice == 'D' || choice == 'd'){
-
+            
         }else if(choice == 'S' || choice == 's'){
+            free(array);
             exit(0);
         }else{
             system("clear");
